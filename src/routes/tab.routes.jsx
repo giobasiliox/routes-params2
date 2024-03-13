54627357adbe2e00307b4af9d1367e16bdd29795
Giovana Basílio <giovana.basilio@aluno.senai.br>
2024-03-13 11:33:59 -0300

    Atualizar rotas de navegação

--- src/routes/tab.routes.jsx
+++ src/routes/tab.routes.jsx
@@ -11,7 +11,9 @@ const Tab = createBottomTabNavigator();
 
 const TabRoutes = () => {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator screenOptions={{ headerShown: false }}
+     
+     >
       <Tab.Screen name="Home" component={Home}
         options={{
           tabBarIcon: ({ focused }) => (<Feather name="home" size={24} color={focused ? "#131313" : "#D6D6D6"} />
@@ -21,16 +23,16 @@ const TabRoutes = () => {
           tabBarInactiveTintColor: "#D6D6D6"
         }} />
       <Tab.Screen name="Profile" component={Profile} initialParams={{user}} options={{
-        tabBarIcon: ({ focused }) => (<Feather name="list" size={24} color={focused ? "#131313" : "#D6D6D6"} />
+        tabBarIcon: ({ focused }) => (<Feather name="user" size={24} color={focused ? "#131313" : "#D6D6D6"} />
         ),
-        tabBarLabel: "Categoria",
+        tabBarLabel: "Perfil ",
         tabBarActiveTintColor: "#131313",
         tabBarInactiveTintColor: "#D6D6D6"
       }} />
       <Tab.Screen name="Category" component={Category} options={{
-        tabBarIcon: ({ focused }) => (<Feather name="user" size={24} color={focused ? "#131313" : "#D6D6D6"} />
+        tabBarIcon: ({ focused }) => (<Feather name="list" size={24} color={focused ? "#131313" : "#D6D6D6"} />
         ),
-        tabBarLabel: "Perfil",
+        tabBarLabel: "Categoria",
         tabBarActiveTintColor: "#131313",
         tabBarInactiveTintColor: "#D6D6D6"
       }} />
